b4803079b7568804a2c6ac2839fcc4d5bbaf65b0
Boris Kim <366793@gmail.com>
2022-05-08 17:44:06 +0900

    feature/layoutMainPageMobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,13 +3,15 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <footer className=''>
-      <p>Учебный проект Яндекс.Практикум х BeatFilm.</p>
-      <a href='https://practicum.yandex.ru'>Яндекс.Практикум</a>
-      <a href='https://practicum.yandex.ru'>Github</a>
-      <a href='https://practicum.yandex.ru'>Facebook</a>
-      <p className=''>
-        &copy; {new Date().getFullYear()} Movies Explorer
+    <footer className='footer'>
+      <p className='footer__text footer__line'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__link-container'>
+        <a className='footer__link' href='https://practicum.yandex.ru' target='_blank' rel='noreferrer'>Яндекс.Практикум</a>
+        <a className='footer__link' href='https://github.com/Borisjaou' target='_blank' rel='noreferrer'>Github</a>
+        <a className='footer__link' href='https://www.facebook.com/BorisKimVLG' target='_blank' rel='noreferrer'>Facebook</a>
+      </div>
+      <p className='footer__text'>
+        &copy; {new Date().getFullYear()}
       </p>
     </footer>
   );
