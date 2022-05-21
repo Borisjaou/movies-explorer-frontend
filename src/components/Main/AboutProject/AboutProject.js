a563d0e4a52963cd550685053a7f89ac44b185c1
Boris Kim <366793@gmail.com>
2022-05-22 00:43:12 +0900

    feature/layoutMainPage

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -5,10 +5,16 @@ function AboutProject() {
   return (
     <section className='project-about'>
       <h2 className='project-about__border'>О проекте</h2>
-      <h3 className='project-about__header'>Дипломный проект включал 5 этапов</h3>
-      <p className='project-about__text'>Составление плана, работу над бэкендом, вёрстку добавление функциональности и финальные доработки</p>
-      <h3 className='project-about__header'>На выполнение диплома ушло 5 недель</h3>
-      <p className='project-about__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+      <div className='project-about__description'>
+        <div className='project-about__details'>
+          <h3 className='project-about__header'>Дипломный проект включал 5 этапов</h3>
+          <p className='project-about__text'>Составление плана, работу над бэкендом, вёрстку добавление функциональности и финальные доработки</p>
+        </div>
+        <div className='project-about__details'>
+          <h3 className='project-about__header'>На выполнение диплома ушло 5 недель</h3>
+          <p className='project-about__text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+      </div>
       <div className='project-about__container'>
         <div className='project-about__term'><p className='term__text'>1 неделя</p></div>
         <div className='project-about__date'><p className='term__text'>4 недели</p></div>
--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Portfolio.css'
-import arrow from '../../../images/anchor_arrow_small1.svg';
+import arrow from '../../../images/anchor_arrow_large.svg';
 
 
 function Portfolio() {
@@ -10,15 +10,15 @@ function Portfolio() {
       <div className='portfolio__container'>
         <div className='portfolio__link-container'>
           <a className='portfolio__link' href='https://ya.ru' target='_blank' rel='noreferrer'>Статичный сайт</a>
-          <img className='portfolio__arrow' src={arrow} alt='стрелка' />
+          <a className='portfolio__link' href='https://ya.ru' target='_blank' rel='noreferrer'><img className='portfolio__arrow' src={arrow} alt='стрелка' /></a>
         </div>
         <div className='portfolio__link-container portfolio__link-border'>
           <a className='portfolio__link' href='https://ya.ru' target='_blank' rel='noreferrer'>Адаптивный сайт</a>
-          <img className='portfolio__arrow' src={arrow} alt='стрелка' />
+          <a className='portfolio__link' href='https://ya.ru' target='_blank' rel='noreferrer'><img className='portfolio__arrow' src={arrow} alt='стрелка' /></a>
         </div>
         <div className='portfolio__link-container'>
           <a className='portfolio__link' href='https://ya.ru' target='_blank' rel='noreferrer'>Одностраничное приложение</a>
-          <img className='portfolio__arrow' src={arrow} alt='стрелка' />
+          <a className='portfolio__link' href='https://ya.ru' target='_blank' rel='noreferrer'><img className='portfolio__arrow' src={arrow} alt='стрелка' /></a>
         </div>
       </div>
     </section>
