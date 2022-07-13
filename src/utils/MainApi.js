36f6a80326ffcefc99f53afe5e31f770d951bc38
Boris Kim <366793@gmail.com>
2022-07-13 23:43:35 +0900

    feature/apiRequests

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,19 @@
-/* eslint no-underscore-dangle: ["error", { "allowAfterThisConstructor": true }] */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+
 /* Запросы к API */
+
+/*
+  - Карточка фильма.
+  - Лайк карточки. сохраняет на наш сервер карточку.
+  - Дизлайк карточкию. Удаляет ее из "Сохраненных", т.е. удаляем ее с сервера.
+  - Регистрация пользователя
+  - Авторизация пользователя
+  - Редакитрование пользователя
+  - Сохраненные фильмы находятся в /movies
+
+*/
 
 class Api {
   constructor(config) {
@@ -13,10 +27,38 @@ class Api {
     }
     return Promise.reject(res.status);
   }
-}
+
+  /* список сохраненных фильмов */
+  listItem() {
+    return fetch(`${this._url}/${'movies'}`, {
+      headers: this._headers,
+      credentials: 'include',
+    }).then(this._checkResponse);
+  }
+
+  getUserInfo() {
+    return fetch(`${this.url}/${'users'}/${'me'}`, {
+      headers: this._headers,
+      credentials: 'include',
+    }).then(this._checkResponse);
+  }
+
+  /* getInitialsCards */
+  getSavedMovies() {
+    return fetch(`${this.url}/${'movies'}`, {
+      headers: this._headers,
+      credentials: 'include',
+    }).then(this._checkResponse);
+  }
+
+  editProfile(name, email) {
+    return fetch()
+  }
+
+} // end of Api
 
 const options = {
-  baseUrl: 'https://api.nomoreparties.co/beatfilm-movies',
+  baseUrl: 'your-movie-explorer.nomoredomains.work',
   headers: {
     'Content-Type': 'application/json',
   },
--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,1 +1,36 @@
 /* Запросы к сервису beatfilm-movies */
+
+/*   Поиска по фильмам. Поиск по названию
+  На запрос приходит весь список фильмов, я должен отфильтровать и осуществить поиск. Для этого нужно написать отдельный компонент */
+
+
+class Search {
+  constructor(config) {
+    this.url = config.baseUrl;
+    this._headers = config.headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(res.status);
+  }
+
+  searcMovie() {
+    return fetch(this.url, {
+      method: 'GET',
+      headers: this._headers,
+      credentials: 'include',
+    }).then(this._checkResponse);
+  }
+}
+
+const options = {
+  baseUrl: 'https://api.nomoreparties.co/beatfilm-movies',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+export const auth = new Search(options);
